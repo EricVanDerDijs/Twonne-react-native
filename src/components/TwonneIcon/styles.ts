8c5c248130fcac2665ebe5381599a635bcaf7cc9
Eric van Der Dijs <eric2vanderdijs@gmail.com>
2019-05-31 13:27:59 -0400

    Aplly correct font to logo

--- src/components/TwonneIcon/styles.ts
+++ src/components/TwonneIcon/styles.ts
@@ -10,7 +10,7 @@ export const styles = (
   },
   text: {
     color: yellowTwonne,
-    fontFamily: 'ExpletusSans-SemiBoldItalic',
+    fontFamily: 'ExpletusSans-BoldItalic',
     fontSize,
   },
 });
