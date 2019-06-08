3f01093b5d1217eb6d7e525d4bf5cf9cfe2c8232
Eric Van Der Dijs <eric2vanderdijs@gmail.com>
2019-06-08 13:24:55 -0400

    Enable absolute imports from src/

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
+const plugins = [
+  [require.resolve('babel-plugin-module-resolver'),
+    {
+      root: ["./"],
+      alias: {
+        "src": "./src"
+      },
+      cwd: "packagejson"
+    },
+  ],
+];
+
+
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins,
 };
